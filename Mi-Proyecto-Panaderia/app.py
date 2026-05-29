3380b8273af81c956e0e0db6615436746daa58a5
renatoperezt64-beep <renatoperezt64@gmail.com>
2026-05-28 19:48:41 -0600

    Add files via upload

--- Mi-Proyecto-Panaderia/app.py
+++ Mi-Proyecto-Panaderia/app.py
@@ -1,18 +1,16 @@
 from flask import Flask, render_template
 import os
 
-# Configuramos la ruta absoluta hacia la carpeta 'templates'
-# Esto le dice a Flask: "Busca 'templates' donde sea que esté este archivo app.py"
-base_dir = os.path.dirname(os.path.abspath(__file__))
-template_dir = os.path.join(base_dir, 'templates')
-
+# Esto le dice a Flask dónde buscar tus archivos HTML, sin importar dónde esté el proyecto
+template_dir = os.path.abspath('templates')
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def inicio():
+    # Render buscará 'index.html' dentro de la carpeta 'templates'
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # Render asigna el puerto automáticamente mediante una variable de entorno
+    # Render asigna el puerto automáticamente, esto es vital
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
